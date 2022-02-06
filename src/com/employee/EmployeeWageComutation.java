8348f3ddcb189838b008ae4a74c9a9751e4f1615
Priya <Priyamadiwalar0007@gmail.com>
2022-02-07 00:09:05 +0530

    added: EmployeeWage

--- src/com/employee/EmployeeWageComutation.java
+++ src/com/employee/EmployeeWageComutation.java
@@ -1,34 +1,45 @@
 package com.employee;
   
 public class EmployeeWageComutation {
-	 public static final int IS_PART_TIME = 1;
-	 public static final int IS_FULL_TIME = 2;
+	 public static final int IS_PART_TIME = 4;
+	 public static final int IS_FULL_TIME = 8;
 	 public static final int EMP_RATE_PER_HOUR= 20;
+	 public static final int empPresent = 1;
+	 public static final int empPartTime = 2;
 
 	public static void main(String[] args) {
 		
-		int empHrs=0;
-		int empWage=0;
 		
+		int dailywage= 0;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if ( empCheck == IS_PART_TIME)
-	        
-		        empHrs = 8;
-		 
-		else if (empCheck == IS_FULL_TIME)
-			
-			    empHrs = 4;
-			
-		else {
-			    empHrs = 0;
-			  
-	         }
+		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 		
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage:" + empWage);
-
-	}
+		switch (empCheck) {
+		
+		case empPresent: {
+			dailywage = IS_FULL_TIME * EMP_RATE_PER_HOUR ;
+			System.out.println("Employee Full Time is Present");
+			System.out.println("Employee Daily Wage Is : "+ dailywage);
+			 
+			break;
+		}
+		
+		case empPartTime:{
+			dailywage =IS_PART_TIME *EMP_RATE_PER_HOUR;
+			System.out.println("Employee Part Time is Present");
+			System.out.println("Employee Part Time Wage Is : "+ dailywage);
+			break;
+		}
+		
+		default:
+			System.out.println("Employee is Absent");
+			break;
+		
+		}
+        
+		
+	}	
 
 }
+
 
