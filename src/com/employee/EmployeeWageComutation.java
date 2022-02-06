4e42769c2a52088f130f26a014a96dcdb0d19346
Priya <Priyamadiwalar0007@gmail.com>
2022-02-06 22:08:14 +0530

    added: employeewage

--- src/com/employee/EmployeeWageComutation.java
+++ src/com/employee/EmployeeWageComutation.java
@@ -3,7 +3,12 @@ package com.employee;
 public class EmployeeWageComutation {
 
 	public static void main(String[] args) {
-		
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if ( empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 
 	}
 
