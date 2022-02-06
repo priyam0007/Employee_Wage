58f5c56b25c552344340d6b36fe4dd96d0907a13
Priya <Priyamadiwalar0007@gmail.com>
2022-02-07 01:17:20 +0530

    added: EmployeeWage

--- src/com/employee/EmployeeWageComutation.java
+++ src/com/employee/EmployeeWageComutation.java
@@ -7,6 +7,8 @@ public class EmployeeWageComutation {
 	 public static final int empPresent = 1;
 	 public static final int empPartTime = 2;
 	 public static final int NUM_OF_WORKING_DAYS = 20;
+	 public static final int MAX_HRS_IN_MONTH = 100;
+
 
 	public static void main(String[] args) {
 		
@@ -18,7 +20,7 @@ public class EmployeeWageComutation {
 		switch (empCheck) {
 		
 		case empPresent: {
-			dailywage = IS_FULL_TIME * EMP_RATE_PER_HOUR * NUM_OF_WORKING_DAYS ;
+			dailywage = IS_FULL_TIME * EMP_RATE_PER_HOUR * MAX_HRS_IN_MONTH;
 			System.out.println("Employee Full Time is Present");
 			System.out.println("Employee Daily 20 Day Wage Is : "+ dailywage);
 			 
@@ -26,7 +28,7 @@ public class EmployeeWageComutation {
 		}
 		
 		case empPartTime:{
-			dailywage =IS_PART_TIME *EMP_RATE_PER_HOUR * NUM_OF_WORKING_DAYS;
+			dailywage = IS_PART_TIME * EMP_RATE_PER_HOUR * MAX_HRS_IN_MONTH;
 			System.out.println("Employee Part Time is Present");
 			System.out.println("Employee Part Time 20 Day Wage Is : "+ dailywage);
 			break;
